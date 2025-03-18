976c9998fb61b11e58e4d0183d1234e3da2a8392
rnadyrshin <athlon640@gmail.com>
2025-03-18 11:26:08 +0000

    Some debugging on the real 13-panel screen

--- main/main.cpp
+++ main/main.cpp
@@ -2,16 +2,17 @@
 
 #define PANEL_RES_X 64    // Number of pixels wide of each INDIVIDUAL panel module. 
 #define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
-#define PANEL_CHAIN 14      // Total number of panels chained one to another
+#define PANEL_CHAIN 13      // Total number of panels chained one to another
+#define PANEL_WIDTH     (PANEL_RES_X * PANEL_CHAIN)
 
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
 extern "C" void app_main() {
     HUB75_I2S_CFG mxconfig(PANEL_RES_X, PANEL_RES_Y, PANEL_CHAIN);
     mxconfig.driver = HUB75_I2S_CFG::FM6124;
-    mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_20M;
+    //mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_20M;
+    mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_16M;
     //mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_15M;
-    //mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_16M;
     //mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_10M;
     //mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_8M;
 
@@ -19,7 +20,7 @@ extern "C" void app_main() {
     mxconfig.double_buff = 1;
 
     mxconfig.setPixelColorDepthBits(3);
-    mxconfig.min_refresh_rate = 100;
+    mxconfig.min_refresh_rate = 200;//255;//100;
 
     dma_display = new MatrixPanel_I2S_DMA(mxconfig);
 
@@ -30,19 +31,34 @@ extern "C" void app_main() {
     dma_display->setTextSize(3);     // size 1 == 8 pixels high
     dma_display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
 
-    //dma_display->setTextColor(dma_display->color565(0xFF, 0x82, 0));
     dma_display->setTextColor(dma_display->color565(0xFF, 0x70, 0));
+
+    bool finish = false;
+    int idx = 0;
+    int len_pix = 0;
+    int idx_start = 0;
 
     while (1)
     {
-        for (int i = 0; i < 900; i += 1)
-        {
-            dma_display->clearScreen();
-            dma_display->setCursor(900 - i, 5);
-            dma_display->print("Test message");
-            dma_display->flipDMABuffer();
-            delay(20);
-        }
-    }
+        dma_display->clearScreen();
+        finish = false;
+        idx = idx_start;
 
+        while (!finish)
+        {
+            dma_display->setCursor(idx, 5);
+            len_pix = dma_display->print("Flipper Hackspace    ");
+            len_pix *= 18;
+            idx += len_pix;
+            if (idx > PANEL_WIDTH)
+                finish = true;
+        }
+
+        dma_display->flipDMABuffer();
+        idx_start--;
+        if (idx_start <= -len_pix)
+            idx_start += len_pix;
+
+        delay(25);
+    }
 }
